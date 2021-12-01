5d8fa12acd3a6737b0a1b8d3d6a7432407459cdd
Jason Xu <Xinwen31415@gmail.com>
2021-12-01 11:02:53 -0600

    fixed postbuild script

--- postbuild.js
+++ postbuild.js
@@ -1,6 +1,8 @@
 const sass = require('sass');
 const fs = require('fs');
 
+fs.mkdirSync('dist/public');
+
 fs.readdirSync('public').map((file) => {
 	if (file.endsWith('scss')) {
 		fs.writeFileSync(`dist/public/${file.split('.').slice(0, -1).join('.')}.css`, sass.renderSync({ file: `public/${file}` }).css);
