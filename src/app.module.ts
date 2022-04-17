4f9c23cc693c2acd711075aa20f1780daacba7b1
Jason Xu <Xinwen31415@gmail.com>
2022-04-16 20:01:26 -0500

    added scraper module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DBService } from './db.service';
 import { DiscordService } from './discord.service';
+import { ScraperModule } from './URTWebScraper/scraper.module';
 
 @Module({
-	imports: [],
+	imports: [ScraperModule],
 	controllers: [AppController],
 	providers: [DiscordService, DBService]
 })
 export class AppModule {}
+
